635bd2156bf3679a2400bcd27f4b3ffa1262e4c0
Durin Tasondock <temgoua484@gmail.com>
2026-02-17 14:00:35 +0100

    feat: supprimer le code inutilisé et simplifier la structure de la page de recherche

--- src/app/(main)/research/page.tsx
+++ src/app/(main)/research/page.tsx
@@ -1,10 +1,8 @@
-import { BlocksIcon } from 'lucide-react'
 import { Metadata } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { researchUnits } from './data'
-import { Title1 } from '@/components'
 
 export const metadata: Metadata = {
   title: 'La Recherche Médicale | Laboratoire Sion',
@@ -80,36 +78,8 @@ export const metadata: Metadata = {
 }
 
 const page = () => {
-  const heroLinks = [{title: "Nos Départements de Recherche", link: "#units", Icon: BlocksIcon}]
-
   return (
     <div className='page relative flex flex-col'>
-      <div className='flex relative'>
-        <div className='w-full relative flex brightness-20 h-full'>
-        <Image alt='research-bg' src="https://images.unsplash.com/photo-1656331797721-b593b8f00297?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=880" className='w-1/2 object-cover h-screen' width={1000} height={1000} />
-        <Image alt='research-bg' src="https://images.unsplash.com/photo-1583607264434-2d8e199b3a17?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1470" className='w-1/2 object-cover h-screen' width={1000} height={1000} />
-        </div>
-        <div className='w-full gap-7 absolute top-0 left-0 padded-x text-white text-center flex-col h-full flex items-center justify-center'>
-          <Title1>🔍 La recherche médicale <span className='font-extralight'>arrive bientôt <br /> chez</span> Sion.</Title1>
-          <p className='max-w-xl text-xl text-gray-200 font-extralight'>Le département de recherche du Laboratoire Sion regroupera des <b className='font-extrabold'>scientifiques, biologistes et radiologues</b> engagés dans la production de <b className='font-extrabold'>connaissances utiles à la pratique médicale,</b>, à la <b className='font-extrabold'>prévention,</b> et à <b className='font-extrabold'>l’amélioration continue de la qualité des analyses.</b></p>
-
-          {heroLinks && (
-            <div className='flex flex-wrap justify-center gap-4 mt-5'>
-              {heroLinks.map(({ title, link, Icon }, index) => (
-                <Link
-                  key={index}
-                  href={link}
-                  className='rounded-full bg-white/20 flex gap-4 hover:bg-white/30 transition px-5 py-2 text-sm font-medium'
-                >
-                  <Icon />
-                  {title}
-                </Link>
-              ))}
-            </div>
-          )}
-        </div>
-      </div>
-
       <div id="units" className='padded-x padded-y flex flex-col items-center'>
         <div className='flex flex-col gap-10 lg:gap-20'>
           <div className='flex m-auto flex-col gap-5 items-center text-center'>
