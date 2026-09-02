c4672680316ddfdaa09cbf83a2f276c16e84f18d
Durin Tasondock <temgoua484@gmail.com>
2026-09-02 06:33:59 +0100

    feat: ajouter de nouveaux fichiers PDF et mettre à jour les chemins d'accès dans les fichiers de données des blogs

--- src/data/blogs/fr.ts
+++ src/data/blogs/fr.ts
@@ -80,7 +80,7 @@ const allArticles = {
         type: "pdf",
         title: "Numération Formule Sanguine (NFS)",
         tag: "numeration-formule-sanguine-nfs",
-        pdfPath: "/documents/NFS bon.pdf",
+        pdfPath: "/documents/la-numeration-formule-sanguine.pdf",
         image: "https://plus.unsplash.com/premium_photo-1723044801071-1fef0817e1c2?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         number: 8,
         date: "2019-01-01",
